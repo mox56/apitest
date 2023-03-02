31004fb59faf66a06884d61d0846d52523bf7c00
mox56 <mouaiad71@outlook.com>
2023-03-02 12:57:39 +0200

    test

---
+++ app/src/main/java/com/vogella/android/retrofitgithub/GithubRepo.java
@@ -0,0 +1,13 @@
+package com.vogella.android.retrofitgithub;
+
+
+public class GithubRepo {
+    String name;
+    String owner;
+    String url;
+
+    @Override
+    public String toString() {
+        return(name + " " +  url);
+    }
+}
